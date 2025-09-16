7c8e5564e2e2664263ee02aeed5ef9820ccb2694
ssnagin <56646530+ssnagin@users.noreply.github.com>
2025-09-16 22:21:50 +0300

    Plane2D works properly

--- public_html/src/ts/index.ts
+++ public_html/src/ts/index.ts
@@ -1,7 +1,9 @@
 'use strict'
 
 import onCounterUpdate from "./modules/counter/counter";
+import DOMColoredPoint from "./modules/types/DOMColoredPoint";
 import CustomForm from "./objects/form/CustomForm";
+import Plane2D from "./objects/plane/Plane2D";
 
 var counterValue: number = 1;
 
@@ -25,4 +27,18 @@ function onDOMContentLoaded() {
     const form = new CustomForm(
         defaultForm!
     );
+
+    // CANVAS
+
+    const canvases : HTMLCanvasElement | null = document.querySelector(".sn-canvas-container:first-child > div > canvas");
+
+    console.log(canvases);
+
+    let testPlane : Plane2D = new Plane2D(5, canvases!);
+
+    testPlane.throwPoint(new DOMColoredPoint("green", 4, 5));
+
+    testPlane.throwPoint(new DOMColoredPoint("red", 1, 1));
+    testPlane.throwPoint(new DOMPoint(5, 4));
+    testPlane.throwPoint(new DOMColoredPoint(undefined, 1, 5));
 }
