350e4fddbf68715775a2c6aa089b18b3d84c9e26
ssnagin <56646530+ssnagin@users.noreply.github.com>
2026-01-28 13:45:44 +0300

    otchet

--- src/main/java/com/ssnagin/coordinates/builders/Point2DRBuilder.java
+++ src/main/java/com/ssnagin/coordinates/builders/Point2DRBuilder.java
@@ -8,10 +8,6 @@ public class Point2DRBuilder {
 
     public static Point2DR build(Object xObj, Object yObj, Object RObj) throws NumberFormatException {
 
-////        float x = Float.parseFloat();
-//        float y = Float.parseFloat(yObj.toString());
-//        float R = Float.parseFloat(RObj.toString());
-
         BigDecimal x = new BigDecimal(xObj.toString());
         BigDecimal y = new BigDecimal(xObj.toString());
         BigDecimal R = new BigDecimal(xObj.toString());
